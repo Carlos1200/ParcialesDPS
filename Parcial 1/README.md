a04f7350f2a6bcbc437567d3f1e9896fc4415dff
Carlos1200 <50189393+Carlos1200@users.noreply.github.com>
2021-09-04 15:40:30 -0600

    Agregando Readme parcial 1

---
+++ Parcial 1/README.md
@@ -0,0 +1,6 @@
+#Parcial 1
+
+Estudiante: **Carlos David Herrera Guardado**.
+Carnet: **HG190072**.
+
+Enlace de documento [Aquí](https://drive.google.com/file/d/1ZXaUB1ZH6o5ELOww5I5DaXxMaDS9q7Yz/view?usp=sharing).
--- Parcial 1/ejercicio3/src/Resultado.js
+++ Parcial 1/ejercicio3/src/Resultado.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export const Resultado = ({resultado}) => {
-    const {
-        numeroMax,
-        numeroMin,
-        condicion1,
-        condicion2
-    }=resultado;
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Resultado</Text>
-            <Text style={styles.info}>Numero Más Grande: {numeroMax}</Text>
-            <Text style={styles.info}>Numero Más Pequeño: {numeroMin}</Text>
-            {condicion1&&<Text style={styles.info}>Sumarle 10 al numero mayor: {condicion1}</Text>}
-            {condicion2&&<Text style={styles.info}>Restarle 5 al numero menor: {condicion2}</Text>}
-        </View>
-    )
-}
-const styles=StyleSheet.create({
-    container:{
-        marginTop:20
-    },
-    title:{
-        fontSize:35,
-        fontWeight:'bold',
-        color: 'white'
-    },
-    info:{
-        fontSize:20,
-        color:'white'
-    }
+  const {numeroMax, numeroMin, condicion1, condicion2} = resultado;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Resultado</Text>
+      <Text style={styles.info}>Numero Más Grande: {numeroMax}</Text>
+      <Text style={styles.info}>Numero Más Pequeño: {numeroMin}</Text>
+      {condicion1 && (
+        <Text style={styles.info}>
+          Sumarle 10 al numero mayor: {condicion1}
+        </Text>
+      )}
+      {condicion2 && (
+        <Text style={styles.info}>
+          Restarle 5 al numero menor: {condicion2}
+        </Text>
+      )}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  info: {
+    fontSize: 20,
+    color: 'white',
+  },
 });
